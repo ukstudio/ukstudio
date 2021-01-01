875974b1e26dec2609df3c73de41f8975ed29121
Yuki AKAMATSU <yuki-akamatsu@cookpad.com>
2021-01-02 03:18:30 +0900

    Update manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,8 +52,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `UKSTUDIO BLOG`,
+        short_name: `UKSTUDIO`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
