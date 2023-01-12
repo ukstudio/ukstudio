4255fa9f5f09f65142f920a8cb8c8e3ed946d578
AKAMATSU Yuki <y.akamatsu@ukstudio.jp>
2023-01-13 00:32:28 +0900

    re-indent

--- .eleventy.js
+++ .eleventy.js
@@ -2,35 +2,35 @@ const dayjs = require('dayjs')
 dayjs.locale('ja')
 
 module.exports = function(eleventyConfig) {
-    const markdownIt = require('markdown-it');
-    const markdownItOptions = {
-        html: true,
-        linkify: true
-    };
+  const markdownIt = require('markdown-it');
+  const markdownItOptions = {
+    html: true,
+    linkify: true
+  };
 
-    const md = markdownIt(markdownItOptions)
-    .use(function(md) {
-        // Recognize Mediawiki links ([[text]])
-        md.linkify.add("[[", {
-            validate: /^\s?([^\[\]\|\n\r]+)(\|[^\[\]\|\n\r]+)?\s?\]\]/,
-            normalize: match => {
-                const parts = match.raw.slice(2,-2).split("|");
-                parts[0] = parts[0].replace(/.(md|markdown)\s?$/i, "");
-                match.text = (parts[1] || parts[0]).trim();
-                match.url = `/${parts[0].trim()}/`;
-            }
-        })
+  const md = markdownIt(markdownItOptions)
+  .use(function(md) {
+    // Recognize Mediawiki links ([[text]])
+    md.linkify.add("[[", {
+      validate: /^\s?([^\[\]\|\n\r]+)(\|[^\[\]\|\n\r]+)?\s?\]\]/,
+      normalize: match => {
+        const parts = match.raw.slice(2,-2).split("|");
+        parts[0] = parts[0].replace(/.(md|markdown)\s?$/i, "");
+        match.text = (parts[1] || parts[0]).trim();
+        match.url = `/${parts[0].trim()}/`;
+      }
     })
+  })
 
-    eleventyConfig.setLibrary('md', md);
-    eleventyConfig.addPassthroughCopy('assets');
-    eleventyConfig.addPassthroughCopy('CNAME');
+  eleventyConfig.setLibrary('md', md);
+  eleventyConfig.addPassthroughCopy('assets');
+  eleventyConfig.addPassthroughCopy('CNAME');
 
-    eleventyConfig.addCollection("notes", function (collection) {
-        return collection.getFilteredByGlob(["notes/**/*.md", "index.md"]);
-    });
+  eleventyConfig.addCollection("notes", function (collection) {
+    return collection.getFilteredByGlob(["notes/**/*.md", "index.md"]);
+  });
 
-    eleventyConfig.addFilter("formatDate", (date,format) => {
-      return dayjs(date).format(format)
-    });
+  eleventyConfig.addFilter("formatDate", (date,format) => {
+    return dayjs(date).format(format)
+  });
 }
